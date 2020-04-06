39f2c82a347a69a198253c6bd371a65d787d05be
Katrina Estall <katrinaestall@Katrinas-MacBook-Air.local>
2020-04-06 12:14:52 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ['queen' , 'queen', 'king', 'king']
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+]
 
 var cardsInPlay =[];
 
@@ -10,12 +31,14 @@ function checkForMatch() {
   }
 };
 
-function flipCard(cardId) {
-	console.log("User flipped" + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-	if (cardsInPlay.length === 2) {
-		checkForMatch();
-	}
+function flipCard(cardId) 
+
+{
+	console.log("User flipped" + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
 };
 flipCard(0);
 flipCard(2);
